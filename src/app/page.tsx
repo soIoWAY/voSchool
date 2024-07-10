b16e85c378468db8a73d3a416a04315a426f296a
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-07-10 14:13:04 +0300

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import About from '@/components/main/about/About'
 import Contacts from '@/components/main/contacts/Contacts'
 import Footer from '@/components/main/footer/Footer'
 import Header from '@/components/main/header/Header'
+import Hero from '@/components/main/hero/Hero'
 
 export default function Home() {
 	return (
 		<div className='w-full'>
 			<Header />
 			<About />
+			<Hero />
 			<Contacts />
 			<Footer />
 		</div>
---
+++ src/components/main/hero/Hero.tsx
@@ -0,0 +1,62 @@
+'use client'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const Hero = () => {
+	// мап масиву і перевірка по індексу
+	const [activeHero, setActiveHero] = useState(0)
+	const images = ['/hero1.jpg', '/hero2.jpg', '/hero3.jpg']
+	return (
+		<div className='w-10/12 m-auto flex justify-center gap-20 mt-24'>
+			<div className='w-5/12 relative overflow-hidden h-96'>
+				{images.map((src, index) => (
+					<Image
+						key={index}
+						src={src}
+						alt='hero'
+						layout='fill'
+						objectFit='cover'
+						className={`absolute transition-opacity duration-500 ${
+							index === activeHero ? 'opacity-100' : 'opacity-0'
+						}`}
+					/>
+				))}
+			</div>
+			<div className='w-5/12 flex flex-col justify-between'>
+				<div
+					className={`flex flex-col gap-5 border-l-[3px]  ${
+						activeHero === 0 ? 'border-black' : 'border-transparent'
+					} pl-5  cursor-pointer`}
+					onClick={() => setActiveHero(0)}
+				>
+					<h2 className='text-3xl font-medium'>Expert English Instruction</h2>
+					<p>High-quality English lessons tailored to your needs</p>
+				</div>
+				<div
+					className={`flex flex-col gap-5 border-l-[3px]  ${
+						activeHero === 1 ? 'border-black' : 'border-transparent'
+					} pl-5  cursor-pointer`}
+					onClick={() => setActiveHero(1)}
+				>
+					<h2 className='text-3xl font-medium'>
+						Personalized Feedback and Progress Tracking
+					</h2>
+					<p>Personalized feedback and progress tracking</p>
+				</div>
+				<div
+					className={`flex flex-col gap-5 border-l-[3px]  ${
+						activeHero === 2 ? 'border-black' : 'border-transparent'
+					} pl-5  cursor-pointer`}
+					onClick={() => setActiveHero(2)}
+				>
+					<h2 className='text-3xl font-medium'>
+						Customized Learning Experience
+					</h2>
+					<p>Engaging and interactive learning materials</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Hero
